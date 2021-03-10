982e0c783cb4b06bfd340504be4b134583f5e3f4
Marc Majcher <majcher@gmail.com>
2021-03-10 15:35:13 -0600

    touchup

--- backend/app.js
+++ backend/app.js
@@ -1,40 +1,51 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const createError = require('http-errors');
+const express = require('express');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const methodOverride = require('method-override');
+const path = require('path');
+const randomstring = require('randomstring');
+const session = require('cookie-session');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 const cardsRouter = require('./routes/cards');
-var app = express();
 
-// view engine setup
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.disable('x-powered-by');
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+app.use(methodOverride('_method'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 
-// catch 404 and forward to error handler
+const sessionMiddleware = session({
+  keys: new Array(Math.ceil(Math.random() * 8) + 8)
+    .fill(0)
+    .map(randomstring.generate),
+});
+app.use(sessionMiddleware);
+
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
